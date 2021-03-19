4f66fa75b560b90241c679cddba74d7c14253c6f
Kudry <kudry995@gmail.com>
2021-03-19 18:32:06 +0100

    TFG-3 Cliente
    - Se ha añadido un objeto Cliente a la escena.
    - Se ha creado un script básico que permite al cliente establecer la conexión con un servidor.

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+using System;
+using System.Text;
+using UnityEngine.UI;
+
+using HybridWebSocket;
+
+public class Client : MonoBehaviour
+{
+    const string IP = "172.20.67.4";        // Server IP
+    const int PORT = 2323;                  // Server Port
+
+    WebSocket webSocket;
+             
+
+    void Start(){
+        createConnection();
+    }
+
+    public void createConnection(){
+        
+        try{
+
+            webSocket = WebSocketFactory.CreateInstance("wss://" + IP + ":" + PORT);
+
+            webSocket.OnOpen += () => {
+                Debug.Log("Conexión con el servidor establecida");
+            };
+
+            webSocket.OnMessage += (byte[] data) => {
+                string messageString = Encoding.UTF8.GetString(data);
+                Debug.Log("Recibido : " + messageString);
+            };
+
+            webSocket.OnError += (string errorMessage) =>{
+                Debug.Log("WebSocket error : " + errorMessage);
+            };
+
+            webSocket.OnClose += (WebSocketCloseCode code) =>{
+                Debug.Log("WebSocket cerrado : " + code.ToString());
+            };
+        
+            webSocket.Connect();
+
+        }catch(Exception exception){
+            Debug.Log("Error " + exception);
+        }
+    }
+
+}
