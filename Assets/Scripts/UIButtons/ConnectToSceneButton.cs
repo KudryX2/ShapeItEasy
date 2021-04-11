fdce5b2de7859ff9aeedff58d159b13b0321dbb0
Kudry <kudry995@gmail.com>
2021-04-11 20:44:13 +0200

    [TFG-7] Avance conectarse a una escena
    - Al pulsar el botón "Connect" en la lista de las escenas, solicitamos al servidor conectarnos a dicha escena, al recibir la aprobación se cambia la escena de unity a la escena para la edición de modelos
    
    #18

---
+++ Assets/Scripts/UIButtons/ConnectToSceneButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;  
+
+
+using UnityEngine.EventSystems; // Event data.
+
+
+public class ConnectToSceneButton : MonoBehaviour, IPointerDownHandler{
+
+    public void OnPointerDown (PointerEventData eventData){
+        GameObject.Find("Client").GetComponent<Scenes>().requestConnect(transform.parent.GetChild(0).GetComponent<Text>().text);  // Scene name
+    }
+}
