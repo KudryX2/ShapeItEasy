a0681fad1a246a529c2a59f92c151d8d98d335e8
Kudry <15013415+KudryX2@users.noreply.github.com>
2021-07-23 11:35:39 +0200

    [TFG-19] Dropdown menu y panel añadir formas
    - Se ha añadido un menú desplegable
    - Se ha añadido un panel para añadir nuevas formas a la escena
    
    #45

---
+++ Assets/Scripts/EditSceneScripts/UI/AddShapePanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AddShapePanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void show(){
+        gameObject.SetActive(true);
+    }
+
+    public void hide(){
+        gameObject.SetActive(false);
+    }
+
+    public void addShapeButton(){
+        Debug.Log("addShape");
+        
+        hide();
+    }
+
+    public void cancelButton(){
+        hide();
+    }
+}
---
+++ Assets/Scripts/EditSceneScripts/UI/Dropdowns/ShapesDropDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShapesDropDown : CustomDropDown
+{
+    [SerializeField]
+    public GameObject addShapePanel;
+
+    public override void clickOnOption(string option){
+        
+        List<Button> optionsList = getOptionsList();
+
+        if(option == optionsList[0].gameObject.name){   // Add Shape -> show add shape panel
+            hideOptions();
+            addShapePanel.GetComponent<AddShapePanel>().show();
+        }
+    }
+}
